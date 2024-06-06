10ee324e8478520055cd7de15d4d5ffe0b1430e9
HystERICKal <nyoroerick12@gmail.com>
2024-06-06 14:36:48 +0200

    fix final errors

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -2,12 +2,12 @@
 """Solve interview puzzle."""
 
 
-def look_next_opened_box(opened_boxes):
+def helper_func(temp_box):
     """Solve interview puzzle."""
-    for index, box in opened_boxes.items():
-        if box.get('status') == 'opened':
-            box['status'] = 'opened/checked'
-            return box.get('keys')
+    for i, singl in temp_box.items():
+        if singl.get('status') == 'opened':
+            singl['status'] = 'opened/checked'
+            return singl.get('keys')
     return None
 
 
@@ -16,31 +16,31 @@ def canUnlockAll(boxes):
     if len(boxes) <= 1 or boxes == [[]]:
         return True
 
-    aux = {}
+    temp_hold = {}
     while True:
-        if len(aux) == 0:
-            aux[0] = {
+        if len(temp_hold) == 0:
+            temp_hold[0] = {
                 'status': 'opened',
                 'keys': boxes[0],
             }
-        keys = look_next_opened_box(aux)
+        keys = helper_func(temp_hold)
         if keys:
             for key in keys:
                 try:
-                    if aux.get(key) and aux.get(key).get('status') \
+                    if temp_hold.get(key) and temp_hold.get(key).get('status') \
                        == 'opened/checked':
                         continue
-                    aux[key] = {
+                    temp_hold[key] = {
                         'status': 'opened',
                         'keys': boxes[key]
                     }
                 except (KeyError, IndexError):
                     continue
-        elif 'opened' in [box.get('status') for box in aux.values()]:
+        elif 'opened' in [singl.get('status') for singl in temp_hold.values()]:
             continue
-        elif len(aux) == len(boxes):
+        elif len(temp_hold) == len(boxes):
             break
         else:
             return False
 
-    return len(aux) == len(boxes)
+    return len(temp_hold) == len(boxes)
