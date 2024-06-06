69467c28794fcafbee275f0e0478b28d6bca02bb
HystERICKal <nyoroerick12@gmail.com>
2024-06-06 14:43:39 +0200

    fix final errors5

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -4,10 +4,10 @@
 
 def helper_func(temp_box):
     """Solve interview puzzle."""
-    for i, box in temp_box.items():
-        if box.get('status') == 'opened':
-            box['status'] = 'opened/checked'
-            return box.get('keys')
+    for x, singl in temp_box.items():
+        if singl.get('status') == 'opened':
+            singl['status'] = 'opened/checked'
+            return singl.get('keys')
     return None
 
 
@@ -16,31 +16,31 @@ def canUnlockAll(boxes):
     if len(boxes) <= 1 or boxes == [[]]:
         return True
 
-    temp_hold = {}
+    aux = {}
     while True:
-        if len(temp_hold) == 0:
-            temp_hold[0] = {
+        if len(aux) == 0:
+            aux[0] = {
                 'status': 'opened',
                 'keys': boxes[0],
             }
-        keys = helper_func(temp_hold)
+        keys = helper_func(aux)
         if keys:
             for key in keys:
                 try:
-                    if temp_hold.get(key) and temp_hold.get(key).get('status') \
+                    if aux.get(key) and aux.get(key).get('status') \
                        == 'opened/checked':
                         continue
-                    temp_hold[key] = {
+                    aux[key] = {
                         'status': 'opened',
                         'keys': boxes[key]
                     }
                 except (KeyError, IndexError):
                     continue
-        elif 'opened' in [box.get('status') for box in temp_hold.values()]:
+        elif 'opened' in [singl.get('status') for singl in aux.values()]:
             continue
-        elif len(temp_hold) == len(boxes):
+        elif len(aux) == len(boxes):
             break
         else:
             return False
 
-    return len(temp_hold) == len(boxes)
+    return len(aux) == len(boxes)
